5423395cbb44e2c5f99da2ca7307cc147344a00d
tatianabio <ismagulova.t@gmail.com>
2023-05-11 19:57:05 -0400

    Added mock.ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,8 @@
-import { useState } from 'react';
 import cx from 'classnames';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
 import './App.css';
-
-interface IPlanet {
-    name: string,
-    rotation_period: number,
-    orbital_period: number,
-    diameter: number,
-    climate: string,
-}
+import { IPlanet, planets } from './mock';
 
 function App() {
-    const planets: IPlanet[] = [
-        {
-            name: 'Tatooine',
-            rotation_period: 23,
-            orbital_period: 304,
-            diameter: 10465,
-            climate: 'arid',
-        },
-        {
-            name: 'Alderaan',
-            rotation_period: 24,
-            orbital_period: 364,
-            diameter: 12500,
-            climate: 'temperate',
-        },
-        {
-            name: 'Yavin IV',
-            rotation_period: 24,
-            orbital_period: 4818,
-            diameter: 10200,
-            climate: 'temperate, tropical',
-        },
-        {
-            name: 'Hoth',
-            rotation_period: 23,
-            orbital_period: 549,
-            diameter: 7200,
-            climate: 'frozen',
-        },
-        {
-            name: 'Dagobah',
-            rotation_period: 23,
-            orbital_period: 341,
-            diameter: 8900,
-            climate: 'murky',
-        },
-    ];
-
     const planetsCount = 60;
 
     const renderInfoPlanets = (infoPlanets: IPlanet[]) => {
