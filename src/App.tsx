a281c0ee592086a1413ab17ebe90f19cc3a56bb6
tatianabio <ismagulova.t@gmail.com>
2023-05-12 19:07:13 -0400

    Added thead and tbody tags into the table and added map method in rendering pagination

--- src/App.tsx
+++ src/App.tsx
@@ -21,39 +21,45 @@ function App() {
         );
     };
 
-    const renderPaginationButton = (totalCount: number, activeNumber: number) => {
+    const renderPaginationButtons = (totalCount: number, activeNumber: number) => {
         const numberPerPage = 10;
         const pagesNumber = Math.ceil(totalCount / numberPerPage);
-        const paginationButtons = [];
 
-        for (let i = 1; i <= pagesNumber; i++) {
-            const isActive = activeNumber === i;
-            paginationButtons.push(
-              <li className='pagination__item'>
-                <button className={cx('pagination__button', isActive && 'pagination__button--active')} type="button">{i}</button>
+        const paginationButtons = new Array(pagesNumber);
+
+        return paginationButtons.map((_, index) => {
+            const isActive = activeNumber === index;
+            const paginationButtonText = index + 1;
+            return (
+              <li key={paginationButtonText} className='pagination__item'>
+                <button className={cx('pagination__button', isActive && 'pagination__button--active')} type="button">{paginationButtonText}</button>
               </li>
             );
-        }
-        return paginationButtons;
+        });
     };
 
   return (
     <section className='planets-section'>
       <h1>Planets</h1>
       <table className='planets-section__table planets-table'>
-        <tr className='planets-table__row'>
-          <th>Name</th>
-          <th>Rotation period</th>
-          <th>Orbital period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-        </tr>
-        {renderInfoPlanets(planets)}
+        <thead>
+          <tr className='planets-table__row'>
+            <th>Name</th>
+            <th>Rotation period</th>
+            <th>Orbital period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderInfoPlanets(planets)}
+        </tbody>
+
       </table>
       <div className='planets-section__pagination-wrapper pagination'>
         <button type="button">Previous page</button>
         <ul className='pagination__buttons'>
-          {renderPaginationButton(planetsCount, 1)}
+          {renderPaginationButtons(planetsCount, 1)}
         </ul>
         <button type="button">Next page</button>
       </div>
