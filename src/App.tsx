6f83648118233dec468d9e98d44e49b4b2b8a38b
tatianabio <ismagulova.t@gmail.com>
2023-05-15 19:40:56 -0400

    Added functions in useGetData

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css';
 import PlanetsTable from './components/PlanetsTable/PlanetsTable';
 import PaginationButtons from './components/PaginationButtons/PaginationButtons';
+import useGetData from './utils/useGetData';
 
 function App() {
+    const { data, isLoading } = useGetData('2');
+    console.log(data);
+    console.log(data.length);
   return (
     <section className='planets-section'>
       <h1>Planets</h1>
--- src/utils/useGetData.ts
+++ src/utils/useGetData.ts
@@ -1,5 +1,24 @@
-const useGetData = (link: string) => {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
+const useGetData = <T>(linkName: string) => {
+ const [isLoading, setIsLoading] = useState(false);
+ const [data, setData] = useState<T[]>([]);
+    const getData = async () => {
+        try {
+            setIsLoading(true);
+            const response = await axios.get(`https://swapi.dev/api/planets/?page=${linkName}`);
+            setData(response.data.results);
+        } catch (e) {
+            console.log('Error', e);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => { getData().then(); }, []);
+
+ return { data, isLoading };
 };
 
 export default useGetData;
