11b8568d1159e9298b9a12a5059db6c16effcdca
tatianabio <ismagulova.t@gmail.com>
2023-05-23 16:46:07 -0400

    Added query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import StyledApp from './styles/StyledApp';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <StyledApp />
+    <QueryClientProvider client={queryClient}>
+      <StyledApp />
+    </QueryClientProvider>
+
   </React.StrictMode>,
 );
