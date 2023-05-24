f33b315dc768def8b00b2ee4dff02f45debccc58
tatianabio <ismagulova.t@gmail.com>
2023-05-24 16:26:54 -0400

    Moved React Query Hook to custom hook useGetData

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useQuery } from '@tanstack/react-query';
 import GlobalStyle from './styles/GlobalStyle';
-import getPlanetsData from './utils/apiFunctions';
 import PlanetsTable from './components/PlanetsTable/PlanetsTable';
 import PaginationButtons from './components/PaginationButtons/PaginationButtons';
+import useGetData from './utils/useGetData';
 
 function App({ className }:{className: string}) {
     const [currentPage, setCurrentPage] = useState<number>(1);
 
-    const { data, isLoading } = useQuery(['planets', currentPage], () => getPlanetsData(`${currentPage}`), { keepPreviousData: true, refetchOnWindowFocus: false });
-
-    const planetsData = data?.results || [];
-    const count = data?.count;
-    const numberPerPage = planetsData?.length || 1;
+    const { planetsArray, planetsCount, isPlanetDataLoading, numberPerPage, pagesNumber } = useGetData(currentPage);
 
     const onClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
         const buttonNumber = (event.target as HTMLButtonElement).textContent;
         setCurrentPage(+buttonNumber);
     };
-
-    const pagesNumber = Math.ceil(count / numberPerPage);
 
     const isFirstPage = currentPage === 1;
     const isLastPage = currentPage === pagesNumber;
@@ -30,14 +23,14 @@ function App({ className }:{className: string}) {
       <GlobalStyle />
       <h1>Planets</h1>
       <div className='container'>
-        <PlanetsTable planetsData={planetsData} />
-        {isLoading && <div className='loading'><span>Loading...</span></div>}
+        <PlanetsTable planetsData={planetsArray} />
+        {isPlanetDataLoading && <div className='loading'><span>Loading...</span></div>}
       </div>
       <div className='pagination-wrapper'>
         <button type="button" disabled={isFirstPage} onClick={() => setCurrentPage(currentPage - 1)}>Previous page</button>
         {
-              count &&
-              <PaginationButtons totalCount={count} activeNumberButton={currentPage} numberPerPage={numberPerPage} onClick={onClickHandler} />
+            planetsCount &&
+              <PaginationButtons totalCount={planetsCount} activeNumberButton={currentPage} numberPerPage={numberPerPage} onClick={onClickHandler} />
           }
         <button type="button" disabled={isLastPage} onClick={() => setCurrentPage(currentPage + 1)}>Next page</button>
       </div>
--- src/utils/useGetData.ts
+++ src/utils/useGetData.ts
@@ -1,28 +1,28 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { IPlanet } from '../mock';
 
-const useGetData = <T>(linkName: string) => {
- const [isLoading, setIsLoading] = useState(false);
- const [data, setData] = useState<T[]>([]);
- const [count, setCount] = useState<number>(0);
- const [numberPerPage, setNumberPerPage] = useState<number>(0);
-    const getData = async () => {
-        try {
-            setIsLoading(true);
-            const response = await axios.get(`https://swapi.dev/api/planets/?page=${linkName}`);
-            setData(response.data.results);
-            setCount(response.data.count);
-            setNumberPerPage(response.data.results.length);
-        } catch (e) {
-            console.log('Error', e);
-        } finally {
-            setIsLoading(false);
-        }
+interface IPlanetsData {
+    results: IPlanet[];
+    count: number;
+}
+
+const useGetData = (currentPage: number) => {
+    const fetchData = async (page: number): Promise<IPlanetsData> => {
+        const { data } = await axios.get(`https://swapi.dev/api/planets/?page=${page}`);
+
+        return data as IPlanetsData;
     };
 
-    useEffect(() => { getData().then(); }, [linkName]);
+    const { data: planetsData, isLoading: isPlanetDataLoading } = useQuery(['planets', currentPage], () => fetchData(currentPage), { keepPreviousData: true, refetchOnWindowFocus: false });
 
- return { data, count, isLoading, numberPerPage };
+    const planetsArray = planetsData?.results || [];
+    const planetsCount = planetsData?.count;
+    const numberPerPage = planetsArray?.length || 1;
+
+    const pagesNumber = Math.ceil(planetsCount / numberPerPage);
+
+ return { planetsArray, planetsCount, isPlanetDataLoading, numberPerPage, pagesNumber };
 };
 
 export default useGetData;
